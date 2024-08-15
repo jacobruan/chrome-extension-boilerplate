eebd55dad72b5c37523ab8cd9a0d89dbeb218e6a
Jacob Ruan <jacobruan@gmail.com>
2024-08-15 09:56:06 +0800

    build ok

--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,2 @@
-/******/ (() => { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
 console.log("hello world!");
-/******/ })()
-;
 //# sourceMappingURL=content.js.map
